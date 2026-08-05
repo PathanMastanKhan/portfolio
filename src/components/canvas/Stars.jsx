c5adc31bf9f7fc74f861e49ea5972d251e57e310
Pathan Mastan Khan <102128839+PathanMastanKhan@users.noreply.github.com>
2026-08-06 01:10:36 +0530

    Update Stars.jsx

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -6,7 +6,7 @@ import * as random from "maath/random/dist/maath-random.esm";
 const Stars = (props) => {
   const ref = useRef();
   const [sphere] = useState(() =>
-    random.inSphere(new Float32Array(5000), { radius: 1.2 })
+    random.inSphere(new Float32Array(2400), { radius: 1.2 })
   );
 
   useFrame((state, delta) => {
@@ -32,7 +32,7 @@ const Stars = (props) => {
 const StarsCanvas = () => {
   return (
     <div className="w-full h-auto absolute inset-0 z-[-1]">
-      <Canvas camera={{ position: [0, 0, 1] }}>
+      <Canvas camera={{ position: [0, 0, 1] }} dpr={[1, 1]}>
         <Suspense fallback={null}>
           <Stars />
         </Suspense>
