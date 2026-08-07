26c99e15c7e3a5dfb47edcbd9790dd9084574486
Pathan Mastan Khan <102128839+PathanMastanKhan@users.noreply.github.com>
2026-08-07 09:55:22 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
       <div
-        className={`z-10 absolute inset-0 top-[140px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`z-10 absolute inset-0 top-[140px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5 pointer-events-none`}
       >
         <div className="flex flex-col justify-center items-center mt-28 md:mt-5">
           <div className="w-5 h-5 rounded-full bg-gold" />
@@ -31,9 +31,10 @@ const Hero = () => {
         </Suspense>
       </div>
 
-      <div className="absolute z-20 xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+      <div className="absolute z-20 xs:bottom-10 bottom-32 w-full flex justify-center items-center pointer-events-none">
         <button
           type="button"
+          className="pointer-events-auto"
           onClick={() =>
             document.getElementById("stats")?.scrollIntoView({ behavior: "smooth" })
           }
